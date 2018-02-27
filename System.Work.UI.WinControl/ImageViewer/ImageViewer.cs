a9c9f9240d1bef9fd549e1c7c402b4e6c6b1ab05
jinlei59 <jinlei@freesense.cn>
2018-02-27 15:17:00 +0800

    添加ImageViewer控件

---
+++ System.Work.UI.WinControl/ImageViewer/ImageViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System.Work.UI.WinControl
+{
+    public partial class ImageViewer : UserControl
+    {
+        public ImageViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
