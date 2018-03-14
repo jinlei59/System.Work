a1c5d9009190a1de84a285764bd690892fa287f8
jinlei59 <jinlei59@163.com>
2018-03-14 15:14:50 +0800

    添加日志测试程序

---
+++ testlogapp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Syste.Work.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testlogapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory.SetLoggerFactory(new NLogFactory());
+            LoggerFactory.CreateInstance().Debug("test debug");
+            LoggerFactory.CreateInstance().LogInfo("test info");
+            LoggerFactory.CreateInstance().LogWarning("test warning");
+            LoggerFactory.CreateInstance().LogError("test error");
+            LoggerFactory.CreateInstance().Fatal("test Fatal");
+        }
+    }
+}
